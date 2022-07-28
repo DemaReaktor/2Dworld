e73fe3c64e4581d5cc8d51479186c1348f6ec10b
Dema <67798322+DemaReaktor@users.noreply.github.com>
2022-07-28 17:49:06 +0300

    CameraTrigger and Viewer
    CameraTrigger triggers, viewer is generally for objects

---
+++ Assets/scripts/CameraTrigger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+}
